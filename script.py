e3c9639c49931de0831badb5179bdc3c745f924f
EldrithcWraith <103373174+EldrithcWraith@users.noreply.github.com>
2023-06-27 16:38:42 +0300

    Add files via upload

---
+++ script.py
@@ -0,0 +1,5 @@
+from os import system, chdir
+import readline
+import time
+
+system("sudo pacman -S xfce4 mousepad ristretto vlc thunar-archive-plugin thunar-media-tags-plugin xfce4-battery-plugin xfce4-clipman-plugin xfce4-cpufreq-plugin xfce4-cpugraph-plugin xfce4-diskperf-plugin xfce4-fsguard-plugin xfce4-genmon-plugin xfce4-mount-plugin xfce4-mpc-plugin xfce4-netload-plugin xfce4-notifyd xfce4-pulseaudio-plugin xfce4-screenshooter xfce4-sensors-plugin xfce4-systemload-plugin xfce4-taskmanager xfce4-time-out-plugin xfce4-timer-plugin xfce4-wavelan-plugin xfce4-verve-plugin xfce4-whiskermenu-plugin xfce4-xkb-plugin")
